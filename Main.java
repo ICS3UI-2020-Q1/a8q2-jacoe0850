6342b4d386244fef105a49b3105f7f63b341ef1c
jacoe0850 <jacoe0850@wrdsb.ca>
2020-11-02 09:11:22 +0000

    better format Finished basic calculator

--- Main.java
+++ Main.java
@@ -10,10 +10,10 @@ public class Main implements Runnable, ActionListener{
   JLabel secondLabel;
   JLabel thirdLabel;
 
-  JButton addButton;
-  JButton subButton;
-  JButton multButton;
-  JButton divButton;
+  JButton additionButton;
+  JButton subtractionButton;
+  JButton multiplicationButton;
+  JButton divisionButton;
 
   JTextField firstInput;
   JTextField secondInput;
@@ -37,67 +37,55 @@ public class Main implements Runnable, ActionListener{
     mainPanel = new JPanel();
     //disable layout helpers
     mainPanel.setLayout(null);
-
     //make labels for the numbers
     firstLabel = new JLabel("First Number");
     secondLabel = new JLabel("Second Number");
     thirdLabel = new JLabel("Result");
-
     //give location and size of labels
     firstLabel.setBounds(170,225,150,20);
     secondLabel.setBounds(170,255,150,20);
     thirdLabel.setBounds(170,285,150,20);
-
     //puts labels on panel
     mainPanel.add(firstLabel);
     mainPanel.add(secondLabel);
     mainPanel.add(thirdLabel);
-
     //initialize buttons
-    addButton = new JButton("add");
-    subButton = new JButton("sub");
-    multButton = new JButton("mult");
-    divButton = new JButton("div");
-
+    additionButton = new JButton("add");
+    subtractionButton = new JButton("sub");
+    multiplicationButton = new JButton("mult");
+    divisionButton = new JButton("div");
     //set action command for the buttons
-    addButton.setActionCommand("add");
-    subButton.setActionCommand("sub");
-    multButton.setActionCommand("mult");
-    divButton.setActionCommand("div");
-
+    additionButton.setActionCommand("add");
+    subtractionButton.setActionCommand("sub");
+    multiplicationButton.setActionCommand("mult");
+    divisionButton.setActionCommand("div");
     //give location and size of buttons
-    addButton.setBounds(170,375,100,40);
-    subButton.setBounds(280,375,100,40);
-    multButton.setBounds(390,375,100,40);
-    divButton.setBounds(500,375,100,40);
-
+    additionButton.setBounds(170,375,100,40);
+    subtractionButton.setBounds(280,375,100,40);
+    multiplicationButton.setBounds(390,375,100,40);
+    divisionButton.setBounds(500,375,100,40);
     //give action listener to the buttons
-    addButton.addActionListener(this);
-    subButton.addActionListener(this);
-    multButton.addActionListener(this);
-    divButton.addActionListener(this);
-
+    additionButton.addActionListener(this);
+    subtractionButton.addActionListener(this);
+    multiplicationButton.addActionListener(this);
+    divisionButton.addActionListener(this);
     //add the buttons to the panel
-    mainPanel.add(addButton);
-    mainPanel.add(subButton);
-    mainPanel.add(multButton);
-    mainPanel.add(divButton);
-
+    mainPanel.add(additionButton);
+    mainPanel.add(subtractionButton);
+    mainPanel.add(multiplicationButton);
+    mainPanel.add(divisionButton);
     //create text fields
     firstInput = new JTextField();
     secondInput = new JTextField();
     result = new JTextField();
-
     //give location and size of textfield
     firstInput.setBounds(370,225,150,20);
     secondInput.setBounds(370,255,150,20);
     result.setBounds(370,285,150,20);
-
     //add the text fields to the panel
     mainPanel.add(firstInput);
     mainPanel.add(secondInput);
     mainPanel.add(result);
-
     //disable the result textfield
     result.setEnabled(false);
     frame.add(mainPanel);
@@ -107,15 +95,12 @@ public class Main implements Runnable, ActionListener{
   public void actionPerformed(ActionEvent e){
     // get the command from the action
     String command = e.getActionCommand();
-
     //get the strings from the textfields
     String firstText = firstInput.getText();
     String secondText = secondInput.getText();
-
     //convert the strings into integers
     int firstNumber = Integer.parseInt(firstText);
     int secondNumber = Integer.parseInt(secondText);
-
     int answer;
     // calculations
     if(command.equals("add")){
@@ -132,7 +117,6 @@ public class Main implements Runnable, ActionListener{
       result.setText("" + answer);
     }
   }
-
   public static void main(String[] args){
     // Creates an instance of our program
     Main gui = new Main();
